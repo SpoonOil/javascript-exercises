7081e63aba6c156a6a53efb4319772fb7f39c333
SpoonOil <nieves.jon.8@gmail.com>
2023-03-21 16:17:04 -0400

    Refactor function for rest arg, pass all tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
-const removeFromArray = function(array, removable) {
-    array.splice(array.indexOf(removable), 1);
+const removeFromArray = function(array, ...removables) {
+    for (const removable of removables) {
+        if (array.includes(removable)) {
+            array.splice(array.indexOf(removable), 1);
+        }
+    }
     return array
 };
 
 // Do not edit below this line
+
 module.exports = removeFromArray;
