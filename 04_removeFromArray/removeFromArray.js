468969c7db7455c749a4fef7623e8855e5d652ab
SpoonOil <nieves.jon.8@gmail.com>
2023-03-21 16:08:49 -0400

    Make basic solution to exercise 4 first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removable) {
+    array.splice(array.indexOf(removable), 1);
+    return array
 };
 
 // Do not edit below this line
