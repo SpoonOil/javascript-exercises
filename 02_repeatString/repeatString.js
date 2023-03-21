3c73a954643b5ad5862e69dbba58707030b1b016
SpoonOil <nieves.jon.8@gmail.com>
2023-03-21 13:22:29 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if (repeats < 0) {
+        return 'ERROR'
+    }
+    let newString = '';
+    for (repeats; repeats > 0; repeats--) {
+        newString+=string;
+    }
+    return newString
 };
 
 // Do not edit below this line
