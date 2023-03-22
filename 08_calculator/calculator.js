4fe9f6ef6b80049092ba27a7c62ed2d2b1f9ac7a
SpoonOil <nieves.jon.8@gmail.com>
2023-03-22 09:37:14 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let total = 0;
+  for (let num of numArray) {
+    total+= num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let total = numArray[0];
+  for (let i = 0; i < numArray.length; i++) { 
+    if (i==0) {continue}
+    total = total*numArray[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {return 1}
+	let numArray = [];
+  for (let i = num; i > 0; i--) {
+    numArray.push(i);
+  }
+  return multiply(numArray)
 };
 
 // Do not edit below this line
