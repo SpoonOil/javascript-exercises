175100395c2e2e4de2d95427e9d22d42c6df7519
SpoonOil <nieves.jon.8@gmail.com>
2023-03-22 10:25:12 -0400

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/[^a-z]/g,"")
+    reversedString = string.split('').reverse().join('');
+    return string == reversedString;
 };
 
 // Do not edit below this line
