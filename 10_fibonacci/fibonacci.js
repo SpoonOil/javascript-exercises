bd28b8a5a0388f6b73402bea10c5d3520d537220
SpoonOil <nieves.jon.8@gmail.com>
2023-03-24 22:19:30 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {return 'OOPS'}
+    let max = +num;
+    fiboSequence = generateFibonacci(max);
+    // return fiboSequence
+    return fiboSequence[max-1];
 };
+
+function generateFibonacci(max) {
+    let fiboSequence = [1, 1]
+    for (let i = 2; i< max; i++) {
+        fiboSequence.push(fiboSequence[i-2] + fiboSequence[i-1]);
+    }
+    return fiboSequence;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
