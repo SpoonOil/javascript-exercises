45d0dbcd7235c8bef0f02b9cb2af185b86353adb
SpoonOil <nieves.jon.8@gmail.com>
2023-03-24 22:36:09 -0400

    Complete all :)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let newPeople = people.sort((a, b) =>  {
+        thisYear = (new Date()).getFullYear();
+        if (!a.yearOfDeath) {
+            return thisYear - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth)
+        } else if (!b.yearOfDeath) {
+            return a.yearOfDeath - a.yearOfBirth - (thisYear- b.yearOfBirth)
+        }
+        return a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth)
+    })
+    newPeople.reverse()
+    return newPeople[0];
 };
 
 // Do not edit below this line
